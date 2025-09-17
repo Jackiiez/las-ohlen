34f4128822c717f2d162ff8c2cfbeafea8fba4de
Jackiiez <u2440308@rts-365.dk>
2025-09-17 16:36:44 +0200

    .

---
+++ src/main.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import { createRoot } from 'react-dom/client'
+import { GloabelContext } from "./gloabelContext";
+import { createBrowserRouter, RouterProvider } from 'react-router'
+import App from './App.jsx'
+
+
+
+
+
+//router hvor vi putter komponenter ind og laver flere sider
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />
+  },
+ 
+  
+  //  {
+  //   path: "/",
+  //   element: <Search />
+  // },
+])
+//vi laver et state som vi ka gemme i vores context
+const MyProvider = ({ children }) => {
+  const [color, setColor] = useState("light");
+  return <GloabelContext.Provider value={{ color, setColor }}>{children}</GloabelContext.Provider>;
+};
+
+
+
+
+
+
+createRoot(document.getElementById('root')).render(
+
+
+
+  //vi putter vores context rundt om vores projekt
+  <MyProvider>
+    <RouterProvider router={router} />
+  </MyProvider>
+
+
+
+)
