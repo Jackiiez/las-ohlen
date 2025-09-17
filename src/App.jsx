f04fc56f704e0b66e06f55362ed0d1171ce69d86
Jackiiez <U2440308@rts.dk>
2025-09-17 19:03:23 +0200

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,35 @@
-import { useState } from 'react'
+
 
 import './App.css'
+import Frontpage from './components/frontpage'
+import NavBar from './components/navbar'
+import Footer from './components/footer'
+import './components/baner.scss';
+import './components/bil.scss';
+import './components/bog.scss';
+import './components/fif.scss';
+import './components/fodboldgolf.scss';
+import './components/fodbold.scss';
+import './components/footer.scss';
+import './components/gallery.scss';
+import './components/hero.scss';
+import './components/lightbox.scss';
+import './components/loeb.scss';
+import './components/nav.scss';
+import './components/salg.scss';
+import './components/sponsors.scss';
+import './components/globals.css';
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-     hej
+    <NavBar/>
+ <Frontpage/>
+ <Footer/>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,19 @@ import { createRoot } from 'react-dom/client'
 import { GloabelContext } from "./gloabelContext";
 import { createBrowserRouter, RouterProvider } from 'react-router'
 import App from './App.jsx'
+import Barcalona from "./components/barcalona.jsx";
+import Baner from "./components/baner.jsx";
+import Bil from "./components/bil.jsx";
+import Bog from "./components/bog.jsx";
+import Broenby from "./components/broenby.jsx";
+import Fif from "./components/fif.jsx";
+import Fodbold from "./components/fodbold.jsx";
+import Fodboldgolf from "./components/fodboldgolf.jsx";
+import Galleri from "./components/galleri.jsx";
+import Liverpool from "./components/liverpool.jsx";
+import Loeb from "./components/loeb.jsx";
+import Sponsors from "./components/sponsors.jsx";
+import Salg from "./components/salg.jsx";
 
 
 
@@ -13,6 +26,53 @@ const router = createBrowserRouter([
   {
     path: "/",
     element: <App />
+  },  {
+    path: "/baner",
+    element: <Baner/>
+  },  {
+    path: "/barcalona",
+    element: <Barcalona />
+  },  {
+    path: "/bil",
+    element: <Bil />
+  },
+    {
+    path: "/bog",
+    element: <Bog />
+  },
+    {
+    path: "/broenby",
+    element: <Broenby />
+  },
+    {
+    path: "/fif",
+    element: <Fif />
+  },
+    {
+    path: "/fodbold",
+    element: <Fodbold />
+  },
+    {
+    path: "/fodboldgolf",
+    element: <Fodboldgolf/>
+  },
+    {
+    path: "/galleri",
+    element: <Galleri/>
+  },
+    {
+    path: "/liverpool",
+    element: <Liverpool/>
+  },
+    {
+    path: "/loeb",
+    element: <Loeb />
+  },  {
+    path: "/sponsors",
+    element: <Sponsors />
+  },  {
+    path: "/salg",
+    element: <Salg/>
   },
  
   
